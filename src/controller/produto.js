1222d29b2e0b0ced1ed0ec0244c15da88d9ce4e2
Felipe Marchioli <fmarchioli@iouu.com.br>
2020-01-27 00:50:21 -0300

    ajuste atualizar produto

--- src/controller/produto.js
+++ src/controller/produto.js
@@ -81,27 +81,6 @@ module.exports = {
         try {
             const dados = req.body;
 
-            //Upload imagem Azure
-            //Cria o blob service
-            const blobSrv = azure.createBlobService(config.azureConnectionString);
-
-            let filename = guid.raw().toString() + '.jpg';
-            let rawData = req.body.fotoProduto;
-            let matches = rawData.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-            let type = matches[1];
-            let buffer = new Buffer(matches[2], 'base64');
-
-            //Salvando a imagem no azure
-            await blobSrv.createBlockBlobFromText('product-images', filename, buffer, {
-                contentType: type
-            }, function (err, result, response) {
-                if (err) {
-                    filename = 'default-product.png';
-                }
-            });
-
-            dados.fotoProduto = 'https://nodestoreteste.blob.core.windows.net/product-images/' + filename;
-
             dados.promocao.horarioPromocao.forEach(item => {
                 
                 let data = Utils.validaHorario(item.horarioAberto, item.horarioFechado)
