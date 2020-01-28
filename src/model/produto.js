5364bb495dae207d9b789499d486c138d728ac12
Felipe Marchioli <fmarchioli@iouu.com.br>
2020-01-27 22:40:14 -0300

    ajuste produto

--- src/model/produto.js
+++ src/model/produto.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const ProdutoSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
     created: Date,
     updated: Date,
     fotoProduto: String,
--- src/routes.js
+++ src/routes.js
@@ -15,4 +15,16 @@ routes.put('/api/v1/restaurante/atualizar', Restaurante.atualizarRestaurante);
 
 routes.delete('/api/v1/restaurante/excluir', Restaurante.excluirRestaurante);
 
+routes.get('/api/v1/restaurante/listar/:cnpj', Restaurante.listarRestaurante);
+
+routes.get('/api/v1/produto/listar/:id', Produto.listarProduto);
+
+routes.get('/api/v1/produto/listarTodos', Produto.listarTodosProduto);
+
+routes.post('/api/v1/produto/cadastrar', Produto.cadastrarProduto);
+
+routes.put('/api/v1/produto/atualizar', Produto.atualizarProduto);
+
+routes.delete('/api/v1/produto/excluir', Produto.excluirProduto);
+
 module.exports = routes; 
