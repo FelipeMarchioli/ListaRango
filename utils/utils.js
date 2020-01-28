616841c6b12f3fff1bb652838fa13ccf37ff1be9
Felipe Marchioli <fmarchioli@iouu.com.br>
2020-01-27 22:59:44 -0300

    ajuste função valida horário

--- utils/utils.js
+++ utils/utils.js
@@ -1,38 +1,36 @@
 function validaHorario(horaAbrir, horaFechar) {
 
-    let dataAberto = new Date(horaAbrir);
-    let dataFechado = new Date(horaFechar);
-    let minAberto = dataAberto.getUTCMinutes();
-    let minFechado = dataFechado.getUTCMinutes();
-    let horaAberto = dataAberto.getUTCHours();
-    let horaFechado = dataFechado.getUTCHours();
+    let minAberto = horaAbrir.split(":")[1];
+    let minFechado = horaFechar.split(":")[1];
+    let horaAberto = horaAbrir.split(":")[0];
+    let horaFechado = horaFechar.split(":")[0];
 
-    if (horaAberto == horaFechado) {
-        if (minFechado < minAberto + 15) {
+    if (parseInt(horaAberto) == parseInt(horaFechado)) {
+        if (parseInt(minFechado) < parseInt(minAberto) + 15) {
 
             return {horaAbrir:"00:00", horaFechar:"00:00", erro:"Horários de funcionamento deve ter um intervalo maior que 15 minutos."};
         }
-    } else if (horaAberto > horaFechado) {
+    } else if (parseInt(horaAberto) > parseInt(horaFechado)) {
 
         return {horaAbrir:"00:00", horaFechar:"00:00", erro:"Horários de funcionamento deve ter um intervalo maior que 15 minutos."};
     }
 
-    if ((minAberto >= 0 && minAberto < 10) && (minFechado >= 0 && minFechado < 10)) {
+    if ((parseInt(minAberto) >= 0 && parseInt(minAberto) < 10) && (parseInt(minFechado) >= 0 && parseInt(minFechado) < 10)) {
 
-        horaAbrir = horaAberto.toString() + ":0" + minAberto.toString();
-        horaFechar = horaFechado.toString() + ":0" + minFechado.toString();
-    } else if (minAberto >= 0 && minAberto < 10) {
+        horaAbrir = horaAberto + ":0" + minAberto;
+        horaFechar = horaFechado + ":0" + minFechado;
+    } else if (parseInt(minAberto) >= 0 && parseInt(minAberto) < 10) {
 
-        horaAbrir = horaAberto.toString() + ":0" + minAberto.toString();
-        horaFechar = horaFechado.toString() + ":" + minFechado.toString();
-    } else if (minFechado >= 0 && minFechado < 10) {
+        horaAbrir = horaAberto + ":0" + minAberto;
+        horaFechar = horaFechado + ":" + minFechado;
+    } else if (parseInt(minFechado) >= 0 && parseInt(minFechado) < 10) {
 
-        horaAbrir = horaAberto.toString() + ":" + minAberto.toString();
-        horaFechar = horaFechado.toString() + ":0" + minFechado.toString();
+        horaAbrir = horaAberto + ":" + minAberto;
+        horaFechar = horaFechado + ":0" + minFechado;
     } else {
 
-        horaAbrir = horaAberto.toString() + ":" + minAberto.toString();
-        horaFechar = horaFechado.toString() + ":" + minFechado.toString();
+        horaAbrir = horaAberto + ":" + minAberto;
+        horaFechar = horaFechado + ":" + minFechado;
     }
 
     return {horaAbrir:horaAbrir, horaFechar:horaFechar, erro:null};
